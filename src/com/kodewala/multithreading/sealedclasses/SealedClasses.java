b126728c53582f271d8370ca54cbc85bd02d082d
Nayandeep <nayandeepsen@gmail.com>
2026-02-28 12:00:00 +0530

    added sealed classes

--- src/com/kodewala/multithreading/sealedclasses/SealedClasses.java
+++ src/com/kodewala/multithreading/sealedclasses/SealedClasses.java
@@ -1,5 +1,26 @@
 package com.kodewala.multithreading.sealedclasses;
 
+sealed class Bank permits HDFC {
+
+}
+
+sealed class HDFC extends Bank permits HDFCPaymentGateway {
+
+}
+
+final class HDFCPaymentGateway extends HDFC {
+
+}
+
+//class Axis extends Bank{
+//	
+//}
+
 public class SealedClasses {
+
+	public static void main(String[] args) {
+		Bank hdfc = new HDFC();
+		HDFC hdfcPaymentGateway = new HDFCPaymentGateway();
+	}
 
 }
